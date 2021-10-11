27996713c67182eca3c5925790fd043151a270d4
Alain Horner <alain@hornerdigital.ch>
2021-10-11 11:47:32 +0200

    login screen styling

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,62 +1,90 @@
 <template>
-  <!-- TODO: use a different layout for this (no sidebar etc.) -->
-  <form
-    class="mt-8 space-y-6"
-    action="#"
-    method="POST"
-    @submit.prevent="handleFormSubmit"
+  <div
+    class="
+      min-h-screen
+      flex
+      items-center
+      justify-center
+      bg-gray-50
+      py-12
+      px-4
+      sm:px-6
+      lg:px-8
+    "
   >
-    <div class="rounded-md shadow-sm -space-y-px">
+    <div class="max-w-md w-full space-y-8">
       <div>
-        <label for="user-name" class="sr-only">UserName</label>
-        <input
-          id="user-name"
-          v-model="userName"
-          name="user-name"
-          type="text"
-          autocomplete="off"
-          required
-          class="login__input rounded-t-md"
-          placeholder="User Name"
+        <img
+          class="mx-auto h-12 w-auto"
+          src="/img/logo.png"
+          alt="Logo SEB Server"
         />
+        <h1
+          ref="focusTarget"
+          class="mt-6 text-center text-3xl font-extrabold text-gray-900"
+        >
+          Sign in
+        </h1>
       </div>
-      <div>
-        <label for="password" class="sr-only">Password</label>
-        <input
-          id="password"
-          v-model="password"
-          name="password"
-          type="password"
-          autocomplete="off"
-          required
-          class="login__input rounded-b-md"
-          placeholder="Password"
-        />
-      </div>
-    </div>
-
-    <div>
-      <button type="submit" class="login__submit">
-        <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-          <!-- Heroicon name: solid/lock-closed -->
-          <svg
-            class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-              clip-rule="evenodd"
+      <form
+        class="mt-8 space-y-6"
+        action="#"
+        method="POST"
+        @submit.prevent="handleFormSubmit"
+      >
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div>
+            <label for="user-name" class="sr-only">UserName</label>
+            <input
+              id="user-name"
+              v-model="userName"
+              name="user-name"
+              type="text"
+              autocomplete="off"
+              required
+              class="login__input rounded-t-md"
+              placeholder="User Name"
             />
-          </svg>
-        </span>
-        Sign In
-      </button>
+          </div>
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input
+              id="password"
+              v-model="password"
+              name="password"
+              type="password"
+              autocomplete="off"
+              required
+              class="login__input rounded-b-md"
+              placeholder="Password"
+            />
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" class="login__submit">
+            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+              <!-- Heroicon name: solid/lock-closed -->
+              <svg
+                class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+            Sign In
+          </button>
+        </div>
+      </form>
     </div>
-  </form>
+  </div>
 </template>
 
 <script>
