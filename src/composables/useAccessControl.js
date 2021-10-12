c31781fceec71337df0b082f62831b3ac481e9a6
Alain Horner <alain@hornerdigital.ch>
2021-10-12 17:19:27 +0200

    refactor useAuth and useAccessControl so the source of truth is solely useAuth

--- src/composables/useAccessControl.js
+++ src/composables/useAccessControl.js
@@ -1,49 +1,9 @@
-import { ref, watch } from "vue";
-import axios from "axios";
+import { readonly, ref } from "vue";
+import { useAuth } from "@/composables/useAuth";
 
-const PRIVILEGES_KEY = "PRIVILEGES";
+const { hasPrivilege, availablePrivileges, availableActions } = useAuth();
 
-const availablePrivileges = {
-  CONFIGURATION_VALUE: "CONFIGURATION_VALUE",
-  CONFIGURATION_ATTRIBUTE: "CONFIGURATION_ATTRIBUTE",
-  ORIENTATION: "ORIENTATION",
-  USER: "USER",
-  CERTIFICATE: "CERTIFICATE",
-  LMS_SETUP: "LMS_SETUP",
-  USER_ACTIVITY_LOG: "USER_ACTIVITY_LOG",
-  CLIENT_EVENT: "CLIENT_EVENT",
-  CONFIGURATION: "CONFIGURATION",
-  CLIENT_CONNECTION: "CLIENT_CONNECTION",
-  EXAM: "EXAM",
-  CONFIGURATION_NODE: "CONFIGURATION_NODE",
-  INSTITUTION: "INSTITUTION",
-  SEB_CLIENT_CONFIGURATION: "SEB_CLIENT_CONFIGURATION",
-};
-
-const availableActions = {
-  NONE: "NONE",
-  READ: "READ",
-  MODIFY: "MODIFY",
-  WRITE: "WRITE",
-};
-
-const actionsMap = {
-  WRITE: ["WRITE", "MODIFY", "READ"], // "WRITE" means user can "WRITE", "MODIFY" and "READ"
-  MODIFY: ["MODIFY", "READ"], // ...
-  READ: ["READ"],
-  NONE: [],
-};
-
-const privileges = ref(null);
 const accessToCurrentRouteGranted = ref(true);
-
-watch(privileges, (newValue) => {
-  if (newValue === null) {
-    window.localStorage.removeItem(PRIVILEGES_KEY);
-  } else {
-    window.localStorage.setItem(PRIVILEGES_KEY, JSON.stringify(newValue));
-  }
-});
 
 const grantAccessToCurrentRoute = () => {
   accessToCurrentRouteGranted.value = true;
@@ -51,101 +11,6 @@ const grantAccessToCurrentRoute = () => {
 
 const denyAccessToCurrentRoute = () => {
   accessToCurrentRouteGranted.value = false;
-};
-
-const fetchAndStorePrivileges = async (authToken) => {
-  try {
-    const responses = await Promise.all([
-      axios({
-        method: "GET",
-        url: "/admin-api/v1/useraccount/me",
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-        },
-      }),
-      axios({
-        method: "GET",
-        url: "/admin-api/v1/info/privileges",
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-        },
-      }),
-    ]);
-
-    const userRoles = responses[0].data.userRoles;
-    const privilegeMap = responses[1].data;
-
-    // prepare privileges
-    const userPrivileges = {};
-    Object.values(availablePrivileges).forEach((privilege) => {
-      userPrivileges[privilege] = {
-        basePrivilege: [],
-        institutionalPrivilege: [],
-        ownershipPrivilege: [],
-      };
-    });
-
-    // map privileges
-    userRoles.forEach((userRole) => {
-      privilegeMap
-        .filter((privilege) => {
-          return privilege.roleTypeKey.userRole === userRole;
-        })
-        .forEach((privilege) => {
-          if (isKnownPrivilege(privilege.roleTypeKey.entityType)) {
-            userPrivileges[privilege.roleTypeKey.entityType].basePrivilege = [
-              ...new Set([
-                ...userPrivileges[privilege.roleTypeKey.entityType]
-                  .basePrivilege,
-                ...actionsMap[privilege.basePrivilege],
-              ]),
-            ];
-            userPrivileges[
-              privilege.roleTypeKey.entityType
-            ].institutionalPrivilege = [
-              ...new Set([
-                ...userPrivileges[privilege.roleTypeKey.entityType]
-                  .institutionalPrivilege,
-                ...actionsMap[privilege.institutionalPrivilege],
-              ]),
-            ];
-            userPrivileges[
-              privilege.roleTypeKey.entityType
-            ].ownershipPrivilege = [
-              ...new Set([
-                ...userPrivileges[privilege.roleTypeKey.entityType]
-                  .ownershipPrivilege,
-                ...actionsMap[privilege.ownershipPrivilege],
-              ]),
-            ];
-          }
-        });
-    });
-
-    // store privileges
-    privileges.value = userPrivileges;
-  } catch (error) {
-    console.error(error);
-    throw new Error("Error while generating privileges");
-  }
-};
-
-const isKnownPrivilege = (privilege) =>
-  Object.values(availablePrivileges).includes(privilege);
-
-const isKnownAction = (action) =>
-  Object.values(availableActions).includes(action);
-
-const hasPrivilege = (realm, privilege, action) => {
-  if (!(isKnownPrivilege(privilege) && isKnownAction(action))) {
-    return false;
-  }
-
-  if (privileges.value === null) {
-    return false;
-  }
-
-  return privileges.value[privilege][realm].includes(action);
 };
 
 const hasBasePrivilege = (privilege, action) => {
@@ -163,33 +28,15 @@ const hasOwnershipPrivilege = (privilege, action) => {
   return hasPrivilege("ownershipPrivilege", privilege, action);
 };
 
-const removeAllPrivileges = () => {
-  privileges.value = null;
-};
-
-const getPersistedPrivilegesData = () => {
-  const persistedPrivileges = window.localStorage.getItem(PRIVILEGES_KEY);
-
-  if (persistedPrivileges) {
-    return JSON.parse(persistedPrivileges);
-  }
-
-  return null;
-};
-
-privileges.value = getPersistedPrivilegesData();
-
 export const useAccessControl = () => {
   return {
-    fetchAndStorePrivileges,
-    accessToCurrentRouteGranted,
+    availablePrivileges: readonly(availablePrivileges),
+    availableActions: readonly(availableActions),
+    accessToCurrentRouteGranted: readonly(accessToCurrentRouteGranted),
     grantAccessToCurrentRoute,
     denyAccessToCurrentRoute,
-    availablePrivileges,
-    availableActions,
     hasBasePrivilege,
     hasInstitutionalPrivilege,
     hasOwnershipPrivilege,
-    removeAllPrivileges,
   };
 };
--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -1,15 +1,45 @@
 import axios from "axios";
 import qs from "qs";
-import { computed, ref, watch } from "vue";
-import { useAccessControl } from "@/composables/useAccessControl";
-
-const { fetchAndStorePrivileges, removeAllPrivileges } = useAccessControl();
+import { computed, readonly, ref, watch } from "vue";
 
 const AUTH_KEY = "AUTH";
+const PRIVILEGES_KEY = "PRIVILEGES";
 
 const authData = ref(null);
+const privileges = ref(null);
 
 const isAuthenticated = computed(() => authData.value !== null);
+
+const availablePrivileges = {
+  CONFIGURATION_VALUE: "CONFIGURATION_VALUE",
+  CONFIGURATION_ATTRIBUTE: "CONFIGURATION_ATTRIBUTE",
+  ORIENTATION: "ORIENTATION",
+  USER: "USER",
+  CERTIFICATE: "CERTIFICATE",
+  LMS_SETUP: "LMS_SETUP",
+  USER_ACTIVITY_LOG: "USER_ACTIVITY_LOG",
+  CLIENT_EVENT: "CLIENT_EVENT",
+  CONFIGURATION: "CONFIGURATION",
+  CLIENT_CONNECTION: "CLIENT_CONNECTION",
+  EXAM: "EXAM",
+  CONFIGURATION_NODE: "CONFIGURATION_NODE",
+  INSTITUTION: "INSTITUTION",
+  SEB_CLIENT_CONFIGURATION: "SEB_CLIENT_CONFIGURATION",
+};
+
+const availableActions = {
+  NONE: "NONE",
+  READ: "READ",
+  MODIFY: "MODIFY",
+  WRITE: "WRITE",
+};
+
+const actionsMap = {
+  WRITE: ["WRITE", "MODIFY", "READ"], // "WRITE" means user can "WRITE", "MODIFY" and "READ"
+  MODIFY: ["MODIFY", "READ"], // ...
+  READ: ["READ"],
+  NONE: [],
+};
 
 watch(authData, (newValue) => {
   if (newValue === null) {
@@ -18,6 +48,32 @@ watch(authData, (newValue) => {
     window.localStorage.setItem(AUTH_KEY, JSON.stringify(newValue));
   }
 });
+
+watch(privileges, (newValue) => {
+  if (newValue === null) {
+    window.localStorage.removeItem(PRIVILEGES_KEY);
+  } else {
+    window.localStorage.setItem(PRIVILEGES_KEY, JSON.stringify(newValue));
+  }
+});
+
+const isKnownPrivilege = (privilege) =>
+  Object.values(availablePrivileges).includes(privilege);
+
+const isKnownAction = (action) =>
+  Object.values(availableActions).includes(action);
+
+const hasPrivilege = (realm, privilege, action) => {
+  if (!(isKnownPrivilege(privilege) && isKnownAction(action))) {
+    return false;
+  }
+
+  if (privileges.value === null) {
+    return false;
+  }
+
+  return privileges.value[privilege][realm].includes(action);
+};
 
 const getPersistedAuthData = () => {
   const persistedAuth = JSON.parse(window.localStorage.getItem(AUTH_KEY));
@@ -33,9 +89,19 @@ const getPersistedAuthData = () => {
   return null;
 };
 
+const getPersistedPrivilegesData = () => {
+  const persistedPrivileges = window.localStorage.getItem(PRIVILEGES_KEY);
+
+  if (persistedPrivileges) {
+    return JSON.parse(persistedPrivileges);
+  }
+
+  return null;
+};
+
 const invalidateUser = () => {
   authData.value = null;
-  removeAllPrivileges();
+  privileges.value = null;
 };
 
 const fetchAndStoreAuthToken = async (username, password) => {
@@ -69,17 +135,98 @@ const fetchAndStoreAuthToken = async (username, password) => {
   }
 };
 
+const fetchAndStorePrivileges = async (authToken) => {
+  try {
+    const responses = await Promise.all([
+      axios({
+        method: "GET",
+        url: "/admin-api/v1/useraccount/me",
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      }),
+      axios({
+        method: "GET",
+        url: "/admin-api/v1/info/privileges",
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      }),
+    ]);
+
+    const userRoles = responses[0].data.userRoles;
+    const privilegeMap = responses[1].data;
+
+    // prepare privileges
+    const userPrivileges = {};
+    Object.values(availablePrivileges).forEach((privilege) => {
+      userPrivileges[privilege] = {
+        basePrivilege: [],
+        institutionalPrivilege: [],
+        ownershipPrivilege: [],
+      };
+    });
+
+    // map privileges
+    userRoles.forEach((userRole) => {
+      privilegeMap
+        .filter((privilege) => {
+          return privilege.roleTypeKey.userRole === userRole;
+        })
+        .forEach((privilege) => {
+          if (isKnownPrivilege(privilege.roleTypeKey.entityType)) {
+            userPrivileges[privilege.roleTypeKey.entityType].basePrivilege = [
+              ...new Set([
+                ...userPrivileges[privilege.roleTypeKey.entityType]
+                  .basePrivilege,
+                ...actionsMap[privilege.basePrivilege],
+              ]),
+            ];
+            userPrivileges[
+              privilege.roleTypeKey.entityType
+            ].institutionalPrivilege = [
+              ...new Set([
+                ...userPrivileges[privilege.roleTypeKey.entityType]
+                  .institutionalPrivilege,
+                ...actionsMap[privilege.institutionalPrivilege],
+              ]),
+            ];
+            userPrivileges[
+              privilege.roleTypeKey.entityType
+            ].ownershipPrivilege = [
+              ...new Set([
+                ...userPrivileges[privilege.roleTypeKey.entityType]
+                  .ownershipPrivilege,
+                ...actionsMap[privilege.ownershipPrivilege],
+              ]),
+            ];
+          }
+        });
+    });
+
+    // store privileges
+    privileges.value = userPrivileges;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error while generating privileges");
+  }
+};
+
 const authenticateUser = async (username, password) => {
   await fetchAndStoreAuthToken(username, password);
   await fetchAndStorePrivileges(authData.value.token);
 };
 
 authData.value = getPersistedAuthData();
+privileges.value = getPersistedPrivilegesData();
 
 export const useAuth = () => {
   return {
-    isAuthenticated,
+    isAuthenticated: readonly(isAuthenticated),
+    availablePrivileges: readonly(availablePrivileges),
+    availableActions: readonly(availableActions),
     authenticateUser,
     invalidateUser,
+    hasPrivilege,
   };
 };
