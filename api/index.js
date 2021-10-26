23d594bc0d965b5d2d680475288b9434a0692733
Alain Horner <alain@hornerdigital.ch>
2021-10-26 12:05:27 +0200

    remove Vercel debug code

--- api/index.js
+++ api/index.js
@@ -11,20 +11,9 @@ app.use(
     target: process.env.API_URL,
     changeOrigin: true,
     onProxyReq(proxyReq) {
-      // TODO: remove debug
-      console.log("***************");
-      console.log(
-        "got a proxy request to oauth api. Gonna add Basic Auth headers..."
-      );
-      console.log(process.env.API_URL);
-      console.log(process.env.BASIC_AUTH_USER);
-      console.log(process.env.BASIC_AUTH_PASSWORD);
-
       const basicAuthHeader = `Basic ${btoa(
         process.env.BASIC_AUTH_USER + ":" + process.env.BASIC_AUTH_PASSWORD
       )}`;
-
-      console.log(basicAuthHeader);
 
       proxyReq.setHeader("Authorization", basicAuthHeader);
     },
@@ -36,18 +25,7 @@ app.use(
   createProxyMiddleware({
     target: process.env.API_URL,
     changeOrigin: true,
-    onProxyReq() {
-      // TODO: remove debug
-      console.log("***************");
-      console.log(
-        "got a proxy request to admin-api. not doing anything except redirecting..."
-      );
-    },
   })
 );
 
 module.exports = app;
-
-// TODO: https://vercel.com/guides/using-express-with-vercel
-// TODO: then use https://github.com/chimurai/http-proxy-middleware
-// example: https://github.com/vercel/next.js/discussions/14057
