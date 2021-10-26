46a9d7f363a279730cb46c3d31da777f6fbb36fd
Alain Horner <alain@hornerdigital.ch>
2021-10-26 19:05:32 +0200

    add sidebar with status toggle action to institution edit page

--- src/views/InstitutionEdit.vue
+++ src/views/InstitutionEdit.vue
@@ -1,22 +1,36 @@
 <template>
-  <view-basic>
-    <form-institution
-      :existing-institution="institutionState"
-    ></form-institution>
-  </view-basic>
+  <view-split label-aside="Additional actions for this institution.">
+    <template #main>
+      <form-institution
+        :existing-institution="institutionState"
+      ></form-institution>
+    </template>
+    <template #aside>
+      <div class="bg-white px-4 py-5 shadow sm:rounded-lg sm:px-6">
+        <h2 class="mb-3">Actions</h2>
+        <toggle-institution-status-action
+          :id="institutionState.id"
+          :active="institutionState.active"
+          @institution:change="fetchInstitutionData"
+        ></toggle-institution-status-action>
+      </div>
+    </template>
+  </view-split>
 </template>
 
 <script>
-import ViewBasic from "@/components/layout/ViewBasic";
 import FormInstitution from "@/components/form/FormInstitution";
+import ViewSplit from "@/components/layout/ViewSplit";
 import { useAPI } from "@/composables/useAPI";
 import { reactive } from "vue";
 import { useRoute } from "vue-router";
+import ToggleInstitutionStatusAction from "@/components/institution/ToggleInstitutionStatusAction";
 
 export default {
   name: "InstitutionCreate",
   components: {
-    ViewBasic,
+    ToggleInstitutionStatusAction,
+    ViewSplit,
     FormInstitution,
   },
   setup() {
@@ -45,6 +59,7 @@ export default {
 
     return {
       institutionState,
+      fetchInstitutionData,
     };
   },
 };
