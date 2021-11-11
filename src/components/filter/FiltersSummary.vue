99fbd8ae6d5ca7414eed2f4cf47aefe5a368b035
Alain Horner <alain@hornerdigital.ch>
2021-11-11 11:19:59 +0100

    fix display bug with active filters on midsize-screens

--- src/components/filter/FiltersSummary.vue
+++ src/components/filter/FiltersSummary.vue
@@ -26,13 +26,13 @@
         leave-from-class="transform scale-100 opacity-100"
         leave-to-class="transform scale-95 opacity-0"
       >
-        <DisclosurePanel class="mb-4 sm:flex gap-2">
+        <DisclosurePanel class="mb-3">
           <active-filter
             v-for="activeFilter in activeFilters"
             :key="activeFilter.field"
             :field="activeFilter.field"
             :value="activeFilter.value"
-            class="mt-2"
+            class="my-1 sm:mr-2"
           ></active-filter>
         </DisclosurePanel>
       </transition>
