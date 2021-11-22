2874534832adb707eeb71a69529bea603c4cb6cd
Alain Horner <alain@hornerdigital.ch>
2021-11-22 17:21:15 +0100

    paging: logic for displaying items and pages

--- src/composables/usePaging.js
+++ src/composables/usePaging.js
@@ -1,4 +1,6 @@
-import { computed } from "vue";
+import { computed, readonly } from "vue";
+
+const itemsPerPage = 10; // TODO: use this for consuming the API
 
 export const usePaging = (state) => {
   const setValues = (currentPage, totalPages) => {
@@ -8,12 +10,71 @@ export const usePaging = (state) => {
     });
   };
 
-  const currentPage = computed(() => state.paging.currentPage);
-  const totalPages = computed(() => state.paging.totalPages);
+  const currentPage = computed(() => parseInt(state.paging.currentPage));
+  const totalPages = computed(() => parseInt(state.paging.totalPages));
+
+  const totalItems = computed(() => {
+    // this is an estimate, because the API doesn't expose this
+    return Math.round(totalPages.value * itemsPerPage - totalPages.value / 2);
+
+    // TODO: if there is only one page, we can show the EXACT number
+  });
+
+  const firstVisibleItem = computed(
+    () => itemsPerPage * currentPage.value - itemsPerPage + 1
+  );
+  const lastVisibleItem = computed(() => itemsPerPage * currentPage.value);
+
+  const prevPage = computed(() =>
+    currentPage.value > 1 ? currentPage.value - 1 : 0
+  );
+
+  const nextPage = computed(() =>
+    currentPage.value < totalPages.value ? currentPage.value + 1 : 0
+  );
+
+  const showFirstPage = computed(() => {
+    return !(
+      totalPages.value <= 1 ||
+      currentPage.value === 1 ||
+      prevPage.value === 1
+    );
+  });
+
+  const showLastPage = computed(() => {
+    return !(
+      currentPage.value === totalPages.value ||
+      nextPage.value === totalPages.value
+    );
+  });
+
+  const showFillerAfterFirstPage = computed(() => {
+    return (
+      showFirstPage.value && prevPage.value !== 0 && prevPage.value - 1 > 1
+    );
+  });
+
+  const showFillerBeforeLastPage = computed(() => {
+    return (
+      showLastPage.value &&
+      nextPage.value !== totalPages.value &&
+      totalPages.value - nextPage.value > 1
+    );
+  });
 
   return {
     setValues,
+    itemsPerPage: readonly(itemsPerPage),
+    firstVisibleItem,
+    lastVisibleItem,
     currentPage,
+    prevPage,
+    nextPage,
     totalPages,
+    totalItems,
+    showFirstPage,
+    showLastPage,
+    showFillerAfterFirstPage,
+    showFillerBeforeLastPage,
   };
 };
