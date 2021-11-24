0b84fbec660830d43c3e5d9a2617d7ba26a9d3ed
Alain Horner <alain@hornerdigital.ch>
2021-11-24 16:28:24 +0100

    ts installation and basic setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,22 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     "eslint:recommended",
     "plugin:vue/vue3-essential",
     "plugin:vue/vue3-strongly-recommended",
     "plugin:vue/vue3-recommended",
     "@vue/prettier",
+    "@vue/typescript",
   ],
+
   parserOptions: {
-    parser: "babel-eslint",
+    parser: "@typescript-eslint/parser",
   },
+
   rules: {},
 };
